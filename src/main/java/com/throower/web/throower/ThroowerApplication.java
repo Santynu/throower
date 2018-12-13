63a8bd48f58ccc28a9a733d0967b5bd83f171767
Santi <santiago@mitulagroup.com>
2018-12-13 19:17:32 +0100

    Proyecto creado con https://start.spring.io/

---
+++ src/main/java/com/throower/web/throower/ThroowerApplication.java
@@ -0,0 +1,14 @@
+package com.throower.web.throower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThroowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThroowerApplication.class, args);
+	}
+
+}
+
